d39cc8a1f17c2023d753bb69318cfa86ea5ef7a8
Niko Dittmar <nikodittmar@gmail.com>
2025-08-09 22:52:00 -0700

    Fix resource bundle installation for v0.5.1

--- Formula/swiftweb.rb
+++ Formula/swiftweb.rb
@@ -3,10 +3,10 @@ class Swiftweb < Formula
 
   homepage "https://github.com/nikodittmar/SwiftWeb"
 
-  url "https://github.com/nikodittmar/SwiftWeb/archive/refs/tags/v0.5.0.tar.gz"
-  version "0.5.0"
+  url "https://github.com/nikodittmar/SwiftWeb/archive/refs/tags/v0.5.1.tar.gz"
+  version "0.5.1"
 
-  sha256 "b8f5a580d712d10bbe0efce78f1de0d0217dea237e068cdfa8c5d8e325849273"
+  sha256 "f1d7b07ac8ea1364a25953e845b3f96eada0368d6a7a81315e51fbd63dc61e65"
 
   depends_on :xcode => ["15.0", :build]
 
@@ -14,5 +14,7 @@ class Swiftweb < Formula
     system "swift", "build", "--configuration", "release", "--disable-sandbox"
 
     bin.install ".build/release/swiftweb"
+
+    bin.install ".build/release/SwiftWeb_SwiftWebGenerator.bundle"
   end
 end
